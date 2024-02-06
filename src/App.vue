5cc01f28cd5e0ab13f67e49a8a956bb2f82d3fa9
tenthid <tenthsut@gmail.com>
2024-02-06 09:20:18 +0800

    idk if this fix the route problem or not

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
 <template>
-  <!-- <component :is="$route.meta.layout"><router-view></router-view></component> -->
-  <web-header></web-header>
-  <router-view></router-view>
-  <web-footer></web-footer>
+  <component :is="$route.meta.layout">
+    <router-view></router-view>
+  </component>
 </template>
 
 <script setup>
-  import WebHeader from './components/header/WebHeader.vue';
-  import WebFooter from './components/footer/WebFooter.vue';
+  
 </script>
